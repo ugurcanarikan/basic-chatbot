d57734e12d0b2de8b78ff0f9b1616558f836dd69
ugurcanarikan <ugurcanarikan95@gmail.com>
2018-09-05 16:26:47 +0300

    training nlu with database

--- src/server.js
+++ src/server.js
@@ -118,6 +118,7 @@ app.post('/upload/',upload.single('file'), async (req, res) => {
 
 app.post('/uploadd/',upload.single('file'), async (req, res) => {
     var file = {};
+    var ret = {};
     await readExcel(file, UPLOAD_DESTINATION + "/" + req.file.filename).then(async () => {
         var url = "mongodb://vca:Abc1234!@ds135952.mlab.com:35952/nlu";
         console.log("Connecting to the database at " + url);
@@ -133,26 +134,45 @@ app.post('/uploadd/',upload.single('file'), async (req, res) => {
                     console.log("New intents have been inserted to the database");
                     resolve(dbo);
                 }
-            })}).then(dbo => {
-                dbo.collection("Intents").find({}, { projection: { _id: 0 } }).toArray(function(err, result) {
-                    if (err){
-                        reject(err);
-                    };
-                    console.log(result);
-                    db.close();
-                  });
-            })
-        });
-        res.send({statusCode: 200});
+            })}).then(async dbo => {
+                var ret = {};
+                await dbo.collection("Intents").find({}, { projection: { _id: 0 } }).toArray()
+            .then(result => {
+                var fileContents = "language: \"en\" \n\n";
+                    fileContents = fileContents + "pipeline: \"spacy_sklearn\"\n\n" ;
+                    fileContents = fileContents + "data: {\n  \"rasa_nlu_data\": {\n    \"common_examples\": [\n";
+                    for(let i = 0; i < result.length; i++){
+                        fileContents = fileContents + "      {\n        \"text\": \"" + result[i].text + "\",\n";
+                        fileContents = fileContents + "        \"intent\": \"" + result[i].intent + "\"\n      },\n";
+                    }
+                    fileContents = fileContents + "    ]\n   }\n}";
+                    fileContents = encode_utf8(fileContents);
+                    console.log("Training file ready");
+                    console.log(fileContents);
+                    ret = {text: fileContents};
+                    return new Promise((resolve, reject) => {resolve(ret)});
+            }).then(async s => {
+                console.log("Making train request to localhost:5000");
+                await trainNLU(s).then(response => {
+                    if(response.statusCode === 200){
+                        console.log("SUCCESS TRAINING");
+                    }
+                    else{
+                        console.error("FAILED TRAINING");
+                    }
+                    res.send({statusCode: response.statusCode});
+                    }).catch(err => {
+                        console.error(err);
+                    });
+            }).catch(err => {
+                console.error(err);
+                reject(err);
+            }); 
+        })});
     }).catch(err => {
         console.error(err);
-        res.send({statusCode: 400});
     })
 })
-
-async function trainWithDatabase(db){
-
-}
 
 /**
  * Trains the nlu unit given the contents of the file
@@ -182,7 +202,7 @@ async function trainWithFile(file){
                 if(err) console.error(err);
             });
         };
-        fileContents = fileContents + "    ]\n  \}\n}";
+        fileContents = fileContents + "    ]\n   }\n}";
         fileContents = encode_utf8(fileContents);
         console.log("Training file ready");
         return {text: fileContents};
@@ -224,7 +244,7 @@ async function initialTraining(){
         });
     }).then(data => {
         fileContents = fileContents + data;
-        fileContents = fileContents + "    ]\n  \}\n}";
+        fileContents = fileContents + "    ]\n   }\n}";
         fileContents = encode_utf8(fileContents);
         console.log("Training file ready");
         return {text: fileContents};
@@ -476,3 +496,670 @@ function readFile(filePath, s){
         }
     });
 }
+
+
+const array1 = [        {
+    text: "yes", 
+    intent: "affirm",
+  }, 
+  {
+    text: "yep", 
+    intent: "affirm",
+  }, 
+  {
+    text: "yeah", 
+    intent: "affirm",
+  }, 
+  {
+    text: "indeed", 
+    intent: "affirm",
+  }, 
+  {
+    text: "that's right", 
+    intent: "affirm",
+  }, 
+  {
+    text: "ok", 
+    intent: "affirm",
+  }, 
+  {
+    text: "great", 
+    intent: "affirm",
+  }, 
+  {
+    text: "right, thank you", 
+    intent: "affirm",
+  }, 
+  {
+    text: "correct", 
+    intent: "affirm",
+  }, 
+  {
+    text: "great choice", 
+    intent: "affirm",
+  }, 
+  {
+    text: "sounds really good", 
+    intent: "affirm",
+  }, 
+  {
+    text: "right", 
+    intent: "affirm",
+  }, 
+  {
+    text: "affirmative", 
+    intent: "affirm",
+  }, 
+  {
+    text: "roger that", 
+    intent: "affirm",
+  }, 
+  {
+    text: "agreed", 
+    intent: "affirm",
+  }, 
+  {
+    text: "alright", 
+    intent: "affirm",
+  }, 
+  {
+    text: "nice", 
+    intent: "affirm",
+  }, 
+  {
+    text: "bye", 
+    intent: "goodbye",
+  }, 
+  {
+    text: "goodbye", 
+    intent: "goodbye",
+  }, 
+  {
+    text: "good bye", 
+    intent: "goodbye",
+  }, 
+  {
+    text: "stop", 
+    intent: "goodbye",
+  }, 
+  {
+    text: "end", 
+    intent: "goodbye",
+  }, 
+  {
+    text: "farewell", 
+    intent: "goodbye",
+  }, 
+  {
+    text: "Bye bye", 
+    intent: "goodbye",
+  }, 
+  {
+    text: "have a good one", 
+    intent: "goodbye",
+  }, 
+  {
+    text: "see you", 
+    intent: "goodbye",
+  }, 
+  {
+    text: "later", 
+    intent: "goodbye",
+  }, 
+  {
+    text: "so long", 
+    intent: "goodbye",
+  }, 
+  {
+    text: "bye bye", 
+    intent: "goodbye",
+  }, 
+  {
+    text: "see you later", 
+    intent: "goodbye",
+  }, 
+  {
+    text: "have a nice day", 
+    intent: "goodbye",
+  }, 
+  {
+    text: "bye bye", 
+    intent: "goodbye",
+  }, 
+  {
+    text: "goodbye then", 
+    intent: "goodbye",
+  }, 
+  {
+    text: "later", 
+    intent: "goodbye",
+  }, 
+  {
+    text: "peace", 
+    intent: "goodbye",
+  }, 
+  {
+    text: "hey", 
+    intent: "greet",
+  }, 
+  {
+    text: "howdy", 
+    intent: "greet",
+  }, 
+  {
+    text: "hey there", 
+    intent: "greet",
+  }, 
+  {
+    text: "hello", 
+    intent: "greet",
+  }, 
+  {
+    text: "hi", 
+    intent: "greet",
+  }, 
+  {
+    text: "good morning", 
+    intent: "greet",
+  }, 
+  {
+    text: "good evening", 
+    intent: "greet",
+  }, 
+  {
+    text: "dear sir", 
+    intent: "greet",
+  }, 
+  {
+    text: "how are you", 
+    intent: "smalltalk",
+  }, 
+  {
+    text: "whatsup", 
+    intent: "smalltalk",
+  }, 
+  {
+    text: "sup", 
+    intent: "smalltalk",
+  }, 
+  {
+    text: "how do you feel", 
+    intent: "smalltalk",
+  }, 
+  {
+    text: "are you ok", 
+    intent: "smalltalk",
+  }, 
+  {
+    text: "how are you doing", 
+    intent: "smalltalk",
+  }, 
+  {
+    text: "how u doin", 
+    intent: "smalltalk",
+  }, 
+  {
+    text: "how is it going", 
+    intent: "smalltalk",
+  }, 
+  {
+    text: "are you all set", 
+    intent: "smalltalk",
+  }, 
+  {
+    text: "thanks", 
+    intent: "thank",
+  }, 
+  {
+    text: "thank you", 
+    intent: "thank",
+  }, 
+  {
+    text: "thx", 
+    intent: "thank",
+  }, 
+  {
+    text: "thank you very much", 
+    intent: "thank",
+  }, 
+  {
+    text: "much appreciated", 
+    intent: "thank",
+  }, 
+  {
+    text: "appreciate that", 
+    intent: "thank",
+  }, 
+  {
+    text: "gratitude", 
+    intent: "thank",
+  }, 
+  {
+    text: "thanks a lot", 
+    intent: "thank",
+  }, 
+  {
+    text: "thank you very much", 
+    intent: "thank",
+  }, 
+  {
+    text: "thanks very much", 
+    intent: "thank",
+  }, 
+  {
+    text: "weather", 
+    intent: "weather",
+  }, 
+  {
+    text: "temperature", 
+    intent: "weather",
+  }, 
+  {
+    text: "rain", 
+    intent: "weather",
+  }, 
+  {
+    text: "rainy", 
+    intent: "weather",
+  }, 
+  {
+    text: "snow", 
+    intent: "weather",
+  }, 
+  {
+    text: "snowy", 
+    intent: "weather",
+  }, 
+  {
+    text: "wind", 
+    intent: "weather",
+  }, 
+  {
+    text: "windy", 
+    intent: "weather",
+  }, 
+  {
+    text: "temp", 
+    intent: "weather",
+  }, 
+  {
+    text: "sky", 
+    intent: "weather",
+  }, 
+  {
+    text: "forecast", 
+    intent: "weather",
+  }, 
+  {
+    text: "what is the weather", 
+    intent: "weather",
+  }, 
+  {
+    text: "how is the weather", 
+    intent: "weather",
+  }, 
+  {
+    text: "how hot it is", 
+    intent: "weather",
+  }, 
+  {
+    text: "is it going to rain", 
+    intent: "weather",
+  }, 
+  {
+    text: "forecast", 
+    intent: "weather",
+  }, 
+  {
+    text: "temp", 
+    intent: "weather",
+  }, 
+  {
+    text: "hot", 
+    intent: "weather",
+  }, 
+  {
+    text: "cold", 
+    intent: "weather",
+  }, 
+  {
+    text: "wet", 
+    intent: "weather",
+  }, 
+  {
+    text: "will it rain", 
+    intent: "weather",
+  }, 
+  {
+    text: "today's weather forecast", 
+    intent: "weather",
+  }, 
+  {
+    text: "money", 
+    intent: "currency",
+  }, 
+  {
+    text: "exchange", 
+    intent: "currency",
+  }, 
+  {
+    text: "rate", 
+    intent: "currency",
+  }, 
+  {
+    text: "dollar", 
+    intent: "currency",
+  }, 
+  {
+    text: "euro", 
+    intent: "currency",
+  }, 
+  {
+    text: "lira", 
+    intent: "currency",
+  }, 
+  {
+    text: "trade", 
+    intent: "currency",
+  }, 
+  {
+    text: "what is the dollar rate", 
+    intent: "currency",
+  }, 
+  {
+    text: "what is the euro rate", 
+    intent: "currency",
+  }, 
+  {
+    text: "how much lira one dollar is", 
+    intent: "currency",
+  }, 
+  {
+    text: "how much lira one euro is", 
+    intent: "currency",
+  }, 
+  {
+    text: "how much dollar", 
+    intent: "currency",
+  }, 
+  {
+    text: "how much euro", 
+    intent: "currency",
+  }, 
+  {
+    text: "how much lira", 
+    intent: "currency",
+  }, 
+  {
+    text: "exchange rates", 
+    intent: "currency",
+  }, 
+  {
+    text: "inflation", 
+    intent: "currency",
+  }, 
+  {
+    text: "loss", 
+    intent: "currency",
+  }, 
+  {
+    text: "gain", 
+    intent: "currency",
+  }, 
+  {
+    text: "bid", 
+    intent: "currency",
+  }, 
+  {
+    text: "openning bid", 
+    intent: "currency",
+  }, 
+  {
+    text: "closing bid", 
+    intent: "currency",
+  }, 
+  {
+    text: "average", 
+    intent: "currency",
+  }, 
+  {
+    text: "open", 
+    intent: "currency",
+  }, 
+  {
+    text: "close", 
+    intent: "currency",
+  }, 
+  {
+    text: "midpoint", 
+    intent: "currency",
+  }, 
+  {
+    text: "dollar rate", 
+    intent: "currency",
+  }, 
+  {
+    text: "euro rate", 
+    intent: "currency",
+  }, 
+  {
+    text: "dollar/tl", 
+    intent: "currency",
+  }, 
+  {
+    text: "euro/tl", 
+    intent: "currency",
+  }, 
+  {
+    text: "usd", 
+    intent: "currency",
+  }, 
+  {
+    text: "eur", 
+    intent: "currency",
+  }, 
+  {
+    text: "tl", 
+    intent: "currency",
+  }, 
+  {
+    text: "value", 
+    intent: "currency",
+  }, 
+  {
+    text: "currency", 
+    intent: "currency",
+  }, 
+  {
+    text: "american dollar", 
+    intent: "currency",
+  }, 
+  {
+    text: "try", 
+    intent: "currency",
+  }, 
+  {
+    text: "retard", 
+    intent: "insult",
+  }, 
+  {
+    text: "idiot", 
+    intent: "insult",
+  }, 
+  {
+    text: "moron", 
+    intent: "insult",
+  }, 
+  {
+    text: "stupid", 
+    intent: "insult",
+  }, 
+  {
+    text: "fuck you", 
+    intent: "insult",
+  }, 
+  {
+    text: "shit", 
+    intent: "insult",
+  }, 
+  {
+    text: "bitch", 
+    intent: "insult",
+  }, 
+  {
+    text: "fuck", 
+    intent: "insult",
+  }, 
+  {
+    text: "fucking", 
+    intent: "insult",
+  }, 
+  {
+    text: "stupid bot", 
+    intent: "insult",
+  }, 
+  {
+    text: "fucking moron", 
+    intent: "insult",
+  }, 
+  {
+    text: "fucking idiot", 
+    intent: "insult",
+  }, 
+  {
+    text: "fucking retard", 
+    intent: "insult",
+  }, 
+  {
+    text: "you moron", 
+    intent: "insult",
+  }, 
+  {
+    text: "stupid little shit", 
+    intent: "insult",
+  }, 
+  {
+    text: "useless bot", 
+    intent: "insult",
+  }, 
+  {
+    text: "waste of time", 
+    intent: "insult",
+  }, 
+  {
+    text: "dirty", 
+    intent: "insult",
+  }, 
+  {
+    text: "imbecile", 
+    intent: "insult",
+  }, 
+  {
+    text: "go fuck yourself", 
+    intent: "insult",
+  }, 
+  {
+    text: "damn", 
+    intent: "insult",
+  }, 
+  {
+    text: "damn you", 
+    intent: "insult",
+  }, 
+  {
+    text: "not working", 
+    intent: "frustration",
+  }, 
+  {
+    text: "not helping", 
+    intent: "frustration",
+  }, 
+  {
+    text: "this is not what I asked", 
+    intent: "frustration",
+  }, 
+  {
+    text: "this is not what I wanted", 
+    intent: "frustration",
+  }, 
+  {
+    text: "wrong", 
+    intent: "frustration",
+  }, 
+  {
+    text: "false", 
+    intent: "frustration",
+  }, 
+  {
+    text: "what is this", 
+    intent: "frustration",
+  }, 
+  {
+    text: "what is that", 
+    intent: "frustration",
+  }, 
+  {
+    text: "I didn't want this", 
+    intent: "frustration",
+  }, 
+  {
+    text: "I asked something different", 
+    intent: "frustration",
+  }, 
+  {
+    text: "not that", 
+    intent: "frustration",
+  }, 
+  {
+    text: "not this", 
+    intent: "frustration",
+  }, 
+  {
+    text: "you were not helpful", 
+    intent: "frustration",
+  }, 
+  {
+    text: "you were not useful", 
+    intent: "frustration",
+  }, 
+  {
+    text: "useless", 
+    intent: "frustration",
+  }, 
+  {
+    text: "inaccurate", 
+    intent: "frustration",
+  }, 
+  {
+    text: "not good", 
+    intent: "frustration",
+  }, 
+  {
+    text: "bad", 
+    intent: "frustration",
+  }, 
+  {
+    text: "terrible", 
+    intent: "frustration",
+  }, 
+  {
+    text: "mistake", 
+    intent: "frustration",
+  }, 
+  {
+    text: "this is not very accurate", 
+    intent: "frustration",
+  }, 
+  {
+    text: "this is inaccurate", 
+    intent: "frustration",
+  }, 
+  {
+    text: "this is not correct", 
+    intent: "frustration",
+  }, 
+  {
+    text: "bad ansver", 
+    intent: "frustration",
+  }          
+]
